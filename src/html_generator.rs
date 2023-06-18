0d1f8dafe5cc4e591c483f563b93188c730b53c3
zacharymnp <56311867+zacharymnp@users.noreply.github.com>
2023-06-18 01:59:35 -0500

    intro to html generation

--- src/html_generator.rs
+++ src/html_generator.rs
@@ -1,3 +1,37 @@
+//  https://www.youtube.com/watch?v=HNnbIW2Kzbc
+
+#[doc = "Basic HTML code for the website"]
+pub fn generate_html(dialog: Option<&str>) -> String {
+    let dialog_html = match dialog {
+        Some(message) => format!("<p>Submitted message: {}</p>", message),
+        None => String::new(),
+    };
+
+    let html = format!(
+        r#"
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>QuADS</title>
+        </head>
+        <body>
+            <h1>QuADS</h1>
+            <p>Questionablly Accurate Degree Simulator</p>
+            {}
+            <form method="post" action="/submit">
+                <label for="message">Enter a message:</label><br>
+                <input type="text" id="message" name="message"><br>
+                <input type="submit" value="Submit">
+            </form>
+        </body>
+        </html>
+        "#,
+        dialog_html
+    );
+
+    html.to_string()
+}
+
 //TODO: Get information to generate a Student item
 
 //TODO: Display feedback post-processing
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,38 @@ mod catalog_scraper;
 mod html_generator;
 mod student;
 
-use crate::catalog_scraper::nav_to_programs;
+use catalog_scraper::nav_to_programs;
+use warp::{Filter, Reply};
+use warp::multipart::FormData;
+use crate::html_generator::generate_html;
 
 #[doc = "Main and stuff"]
-fn main() {
+#[tokio::main]
+async fn main() {
     #![allow(unused_variables)]
     #![allow(dead_code)]
 
-    //TODO: Generate site to gather information to build a student
+    //defining the root route
+    let html_route = warp::path::end()
+        .and(warp::get())
+        .map(|| {
+            let html = generate_html(None);
+            warp::reply::html(html)
+        });
+    //defining the submit route
+    let submit_route = warp::path("submit")
+        .and(warp::post())
+        .map(|| {
+            warp::reply::html("Submitted")
+        });
+    //combining routes into a single filter
+    let routes = html_route.or(submit_route);
+    //starting the web server at 'http://localhost:8080'
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 
     //TODO: Using Student, request HTML from the specific year's catalog home page
-    let home = catalog_scraper::request_html("https://catalog.rpi.edu/index.php").expect("fetching homepage failed");
-    let programs = nav_to_programs(&home).expect("fetching programs failed");
+//    let home = catalog_scraper::request_html("https://catalog.rpi.edu/index.php").expect("fetching homepage failed");
+//    let programs = nav_to_programs(&home).expect("fetching programs failed");
 
     //TODO: Check thru each of Student's majors, paying attention to concentration
 
